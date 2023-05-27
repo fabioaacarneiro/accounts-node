1289610a0874f3dd091b312b3cde866f702ee196
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-05-27 17:29:20 -0300

    create buildAccount function and your issues

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import chalk from "chalk"
 //modulos internos
 import fs from "fs"
 
-(function operation() {
+const operation = () => {
     inquirer.prompt([{
         type: "list",
         name: "action",
@@ -21,12 +21,40 @@ import fs from "fs"
         const action = answer['action']
 
         if (action == "Criar Conta") createAccount()
-        
+
     }).catch((err) => console.log(err))
-})()
+}
 
 //create an account
 const createAccount = () => {
     console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"))
     console.log(chalk.green("Defina as opções da sua conta a seguir"))
+    buildAccount()
 }
+
+const buildAccount = () => {
+    inquirer.prompt([{
+        name: "accountName",
+        message: "Digite um nome para a sua conta:",
+    }]).then((answer) => {
+        const accountName = answer["accountName"]
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+        
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black("Esta conta já existe, use outro nome"))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{\n\t"blance": 0\n}', (err) => console.log(err))
+        console.log("Parabéns, sua conta foi criada com sucesso!")
+        operation()
+
+    }).catch((err) => console.log(err))
+}
+
+operation()

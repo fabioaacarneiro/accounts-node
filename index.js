d37c76e89a056413e2fdf7b1ddaecbb7c78f750d
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-05-27 17:14:27 -0300

    create a createAccount function

--- index.js
+++ index.js
@@ -19,5 +19,14 @@ import fs from "fs"
         ],
     }]).then((answer) => {
         const action = answer['action']
+
+        if (action == "Criar Conta") createAccount()
+        
     }).catch((err) => console.log(err))
 })()
+
+//create an account
+const createAccount = () => {
+    console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"))
+    console.log(chalk.green("Defina as opções da sua conta a seguir"))
+}
